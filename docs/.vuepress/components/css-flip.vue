41f81af2dee71a32461336c6023cd7c0ae8de808
楚锐 <cn5508@163.com>
2021-02-01 14:34:31 +0800

    【新增】 1. 增加css动画模板

--- docs/.vuepress/components/css-flip.vue
+++ docs/.vuepress/components/css-flip.vue
@@ -2,33 +2,28 @@
     <div class="css-flip">
         <!-- 基础节点 -->
         <div class="wrap">
-            <div v-for="item in imgList" class="img-wrap" ref="imgRef" :key="item">
-                <img :src="item" />
+            <div v-for="item in imgList" class="img-wrap" ref="imgRef" :key="item.id">
+                <img :src="require('../public/'+ item.url +'.jpg')" />
             </div>
         </div>
 
         <!-- 事件按钮 -->
         <button @click="random">乱序</button>
+        <button @click="add">添加</button>
+        <button @click="remove">删除</button>
     </div>
 </template>
 
 <script>
 export default {
+    name: 'css-flip',
     data() {
         return {
-            imgList: [
-                '/test-image (1).jpg',
-                '/test-image (2).jpg',
-                '/test-image (3).jpg',
-                '/test-image (4).jpg',
-                '/test-image (5).jpg',
-                '/test-image (6).jpg',
-                '/test-image (7).jpg',
-                '/test-image (8).jpg',
-            ]
+            imgList: []
         }
     },
     methods: {
+        // 获取元素边界
         getBounding() {
             return this.imgRef.map(node => {
                 let rect = node.getBoundingClientRect()
@@ -36,8 +31,39 @@ export default {
                 return { top, left }
             })
         },
+        // 随机添加元素
+        add() {
+            let random = Math.floor(Math.random() * (8 - 1) + 1)
+
+            this.imgList.splice(random, 0, {
+                id: this.uuid(),
+                url: 'test-image ('+ random +')',
+            })
+        },
+        // 随机元素位置
         random() {
             this.imgList.sort(() => Math.random() - 0.5)
+        },
+        // 随机删除元素
+        remove() {
+            let random = Math.floor(Math.random() * (8 - 1) + 1)
+
+            this.imgList.splice(random, 1)
+        },
+        // 生成唯一id
+        uuid() {
+            function S4() {
+                return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+            }
+            return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+        }
+    },
+    created() {
+        for( let i = 1 ; i < 9 ; i++) {
+            this.imgList.push({
+                id: this.uuid(),
+                url: 'test-image ('+i+')',
+            })
         }
     },
     mounted () {
@@ -46,11 +72,16 @@ export default {
     },
     watch: {
         imgList() {
+            this.imgRef = this.$refs.imgRef
+            // 获取元素位移前位置
             let prevPositions = this.getBounding()
             this.$nextTick(() => {
+                // 获取元素位移后位置
                 let currentPositions = this.getBounding()
 
                 this.imgRef.forEach((imgRef, imgIndex) => {
+                    // 防止在新增元素时获取不到报错问题
+                    if (!prevPositions[imgIndex]) return 
                     // 当前图片前后位置偏移坐标计算
                     const currentPosition = currentPositions[imgIndex]
                     const prevPosition = prevPositions[imgIndex]
---
+++ docs/css/animation.md
@@ -0,0 +1,7 @@
+# animation
+
+## FLIP
+<css-flip/>
+<<< @/docs/.vuepress/components/css-flip.vue
+
+
